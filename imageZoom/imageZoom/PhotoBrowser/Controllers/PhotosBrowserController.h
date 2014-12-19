c20b8c470e0da1c413756b8aa20fe011c41c90e4
孙国志 <guozhi.sun@yolu-inc.com>
2014-12-19 19:41:06 +0800

    添加注释

--- imageZoom/imageZoom/PhotoBrowser/Controllers/PhotosBrowserController.h
+++ imageZoom/imageZoom/PhotoBrowser/Controllers/PhotosBrowserController.h
@@ -10,10 +10,22 @@
 typedef void (^finishBlock)(BOOL isCurrent);
 
 @interface PhotosBrowserController : UIViewController
-
+/**
+ * 用于存放滚动视图中的viewController
+ */
 @property (nonatomic, strong) NSMutableArray *zoomViewControllers;
+
+/**
+ * 用于存放PhotoItem对象的数组
+ */
 @property (nonatomic, strong) NSArray *photos;
 
+/**
+ * 用于创建并初始化一个PhotoBrowserController
+ * @param imageArr 一个存放PhotoItem的数组
+ * @param index 当前打开的是第几张图片的放大图
+ * @param block 回调
+ */
 - (instancetype)initWithImageArr:(NSArray *)imageArr imageIndex:(NSInteger)index finish:(finishBlock)block;
 
 @end
